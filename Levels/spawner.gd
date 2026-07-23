9b72019ec37db4df75f348c0ce8264d06a1d917f
robopugo <reimacdougall@gmail.com>
2026-07-23 13:19:22 -0500

    added spawner

---
+++ Levels/spawner.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@onready var enemy_template: Node2D = $"../Enemy"
+@onready var timer: Timer = $Timer
+
+func _ready() -> void:
+	enemy_template.visible = false
+	enemy_template.set_process(false)
+	enemy_template.set_physics_process(false)
+	timer.timeout.connect(_on_timer_timeout)
+
+func _on_timer_timeout() -> void:
+	spawn_enemy()
+
+func spawn_enemy() -> void:
+	if not enemy_template:
+		return
+	
+	var enemy_clone = enemy_template.duplicate()
+	
+	enemy_clone.global_position = global_position
+	
+	get_tree().current_scene.add_child(enemy_clone)
+	
+	enemy_clone.visible = true
+	enemy_clone.set_process(true)
+	enemy_clone.set_physics_process(true)
