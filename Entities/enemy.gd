edaf64b3b7114b1313eae78fa99c3494176246ed
Moovintwo <111332273+MoovinTwo@users.noreply.github.com>
2026-07-23 21:08:59 +0900

    Revert "Merge remote-tracking branch 'origin/new-triangle-controls'"
    This reverts commit b2c5c7c5b7dda3e79f1788347110de4d4011fcc6, reversing
    changes made to 6715b0eed70f326fefbae515d56046f560a79eb1.

--- Entities/enemy.gd
+++ Entities/enemy.gd
@@ -5,16 +5,20 @@ const SPEED = 50
 
 func _ready() -> void:
 	player = $"../Triangle"
-	add_to_group("enemy")
 
+#I set the enemies up so they can only be able to sense the player, and just check if it enters and kills itself.
+#We might want to replace this, but it should allow us to quickly test.
 func _on_kill_zone_body_entered(body: Node2D) -> void:
-	die()
-
-func die():
 	queue_free()
 
+#The player calls this signal, however it doesn't call it yet. Maybe we can do on body enter check if has this?
+func die():
+	#Put animation here when done!
+	queue_free()
+
+#walk to player
 func _physics_process(delta: float) -> void:
-	if player:
-		direction = (player.global_position - global_position).normalized()
-		velocity = direction * SPEED
+	direction = (player.global_position - position).normalized
+	#I'm too tired to figure this out, but basically it needs to do this equation and itll work.
+#	velocity = (direction * SPEED) * delta
 	move_and_slide()
--- Entities/triangle.gd
+++ Entities/triangle.gd
@@ -1,100 +1,41 @@
 extends CharacterBody2D
+
 @export var speed = 300.0
 @export var dash_speed = 3000
 @export var friction = 2
-@export var bounce_speed_retention = 0.6
-@export var stun_duration = 0.5
+
 var dashing = false
 var direction: Vector2 = Vector2.ZERO
-var dash_direction: Vector2 = Vector2.RIGHT
 var can_move = true
 var can_dash = true
-enum Mode { DASH, BOUNCE }
-var mode: Mode = Mode.DASH
-@onready var sprite: AnimatedSprite2D = $Sprite2D
-@onready var stun_timer: Timer = $stunt_timer
-
-func _ready() -> void:
-	stun_timer.one_shot = true
-	if not stun_timer.timeout.is_connected(_on_stun_timer_timeout):
-		stun_timer.timeout.connect(_on_stun_timer_timeout)
 
 @warning_ignore("unused_parameter")
 func _process(delta: float) -> void:
-	if not dashing:
-		look_at(get_global_mouse_position())
-
+	look_at(get_global_mouse_position())
+	
 @warning_ignore("unused_parameter")
 func _physics_process(delta: float) -> void:
 	direction = Input.get_vector("left","right","up","down")
 	if Input.is_action_just_pressed("dash") and can_dash:
 		dashing = true
-		dash_direction = (get_global_mouse_position() - global_position).normalized()
-		rotation = dash_direction.angle()
 		$AudioStreamPlayer2D.play()
 		can_dash = false
 		$dash_timer.start()
 		$dash_cooldown.start()
-	if Input.is_action_just_pressed("toggle_mode"):
-		_toggle_mode()
+		
 	if can_move:	
 		if dashing:
-			velocity = dash_speed * dash_direction
+			var vector = get_global_mouse_position() - global_position
+			velocity = dash_speed * vector.normalized()
 		else:
 			velocity = speed * direction
-	else:
-		velocity = Vector2.ZERO
 	move_and_slide()
-	_handle_wall_collisions()
+	
 
-func _toggle_mode() -> void:
-	if mode == Mode.DASH:
-		mode = Mode.BOUNCE
-		if sprite.sprite_frames and sprite.sprite_frames.has_animation("bounce"):
-			sprite.play("bounce")
-	else:
-		mode = Mode.DASH
-		if sprite.sprite_frames and sprite.sprite_frames.has_animation("dash"):
-			sprite.play("dash")
-
-func _handle_wall_collisions() -> void:
-	if not dashing:
-		return
-	for i in get_slide_collision_count():
-		var collision := get_slide_collision(i)
-		var collider := collision.get_collider()
-		var normal := collision.get_normal()
-
-		if collider and collider.is_in_group("enemy") and mode == Mode.DASH:
-			_kill_enemy(collider)
-			continue
-
-		if mode == Mode.BOUNCE:
-			velocity = velocity.bounce(normal) * bounce_speed_retention
-			dash_direction = velocity.normalized()
-			rotation = dash_direction.angle()
-		else:
-			dashing = false
-			velocity = Vector2.ZERO
-			_apply_stun()
-		break
-
-func _kill_enemy(enemy: Node) -> void:
-	if enemy.has_method("die"):
-		enemy.die()
-	else:
-		enemy.queue_free()
-
-func _apply_stun() -> void:
-	can_move = false
-	stun_timer.stop()
-	stun_timer.start(stun_duration)
-
-func _on_stun_timer_timeout() -> void:
-	can_move = true
 
 func _on_dash_timer_timeout() -> void:
-	dashing = false
+	dashing = false # Replace with function body.
+
 
 func _on_dash_cooldown_timeout() -> void:
 	can_dash = true
