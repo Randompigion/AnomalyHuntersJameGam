1db02ba68633e164a0ad38f0fb96d84e25e72e14
Bullfrog <thebullfrog2709@gmail.com>
2026-07-24 17:06:07 +0800

    Cursor now changes texture based on triangle mode

--- Systems/Cursor/cursor.gd
+++ Systems/Cursor/cursor.gd
@@ -1,6 +1,12 @@
 extends Sprite2D
+var player
+var counter = 0
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position = get_global_mouse_position()
+	if Input.is_action_just_pressed("toggle_mode"):
+		counter += 1
+	if counter % 2 == 0:
+		$AnimatedSprite2D.animation = 'Dash'
+	else:
+		$AnimatedSprite2D.animation = 'Bounce'
