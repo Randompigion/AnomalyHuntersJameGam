3bdfff0ccf98b223911be9a2338fbca345590b1b
Randompigion <aahilahsan@outlook.com>
2026-07-25 22:56:17 -0400

    Adds in collision and finished the level layout

--- Entities/corruptionscene.gd
+++ Entities/corruptionscene.gd
@@ -1,4 +1,5 @@
 extends Node2D
+var triggeredbefore = false
 
 @export var target_scene_path: String = "res://Levels/change_scene.tscn"
 @export var only_player: bool = true
@@ -10,6 +11,8 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if target_scene_path == "":
 		push_warning("SceneChangeZone: no se ha asignado target_scene_path")
 		return
+	if triggeredbefore == false:
+		triggeredbefore = true
 		$waitdidhespillwater.play()
 		await $waitdidhespillwater.finished
-	get_tree().change_scene_to_file.call_deferred(target_scene_path)
+		get_tree().change_scene_to_file.call_deferred(target_scene_path)
